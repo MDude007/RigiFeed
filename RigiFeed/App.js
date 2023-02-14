8361b7a11b99c56ede6f5ab55d0ef83c292d56e9
Mayur Dudhe <mayurdudhe@Mayurs-MacBook-Pro.local>
2023-02-14 16:25:21 +0530

    project setup done

---
+++ RigiFeed/App.js
@@ -0,0 +1,12 @@
+import { SafeAreaView } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <HomeScreen />
+    </SafeAreaView>
+  )
+}
+
+export default App;
